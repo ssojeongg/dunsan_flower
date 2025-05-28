acecf01d99ef84b1f9ddc44245044393c3909d25
Park-Sojeong <qkrthwjd0209@naver.com>
2025-05-28 13:34:32 +0900

    FloatingBtn

--- src/components/FloatingBtn.jsx
+++ src/components/FloatingBtn.jsx
@@ -1,22 +1,43 @@
-import { FaPhone, FaMapMarkerAlt, FaChevronUp } from "react-icons/fa";
+import { FaPhone, FaMapMarkerAlt, FaChevronUp,FaEllipsisV,FaTimes } from "react-icons/fa";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faGift, faB } from '@fortawesome/free-solid-svg-icons';
 import '../assets/css/FloatingBtn.css'
 import '../assets/responsive/R_FloatingBtn.css'
 
+
 import useSideMenuStore from '../store/sideMenuStore'
+import { useState } from "react";
 
 const FloatingBtn = () => {
   const { sideMenuOpen } = useSideMenuStore();
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 767);
 
  const scrollToTop = () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
 
+const openModal = () => setIsOpen(true);
+const closeModal = () => setIsOpen(false);
+
+const toggleFab = () => {
+    setIsOpen(prev => !prev);
+  };
+
   return (
   <div className="FloatingBtn">
+
+    {/* 배경 블러 오버레이 */}
+      <div className={`fab_overlay ${isOpen ? 'show' : ''}`} onClick={toggleFab} />
+
     <div className="fab_container">
-        <div className="fab_buttons">
+      {isMobile && (
+          <button className="fab_main" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <FaTimes /> : <FaEllipsisV />}
+          </button>
+        )}
+        {isMobile && (
+        <div className={`fab_buttons ${isOpen ? "show" : "close"}`}>
           <button className="fab_item">
             <FaPhone />
             <span className="fab_text">대표번호</span>
@@ -38,7 +59,12 @@ const FloatingBtn = () => {
             <span className="fab_text">블로그</span>
           </button>
         </div>
+        )}
     </div>
+
+    
+
+
       {/* pc 버전 */}
       <div className={`fab_pc_container ${sideMenuOpen ? 'width' : ''}`}>
       <div className="fab_pc_buttons">
