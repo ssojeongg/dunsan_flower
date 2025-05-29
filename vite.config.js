64015e314770598e705c4289c690a23e05d42075
Park-Sojeong <qkrthwjd0209@naver.com>
2025-05-30 04:45:02 +0900

    favicon modify

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import ViteFaviconsPlugin from 'vite-plugin-favicon';
 
 const isProdOnGhPages = process.env.VITE_DEPLOY_TARGET === 'GH_PAGES';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteFaviconsPlugin({
+      logo: "favicon.ico",
+      favicons: {
+        path: "/"
+      },
+    })
+  ],
   base: isProdOnGhPages ? '/dunsan_flower/' : '/',
 });
