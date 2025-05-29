b6cbb57b4cc525b80b0004e35d8a0bd6780f8510
Park-Sojeong <qkrthwjd0209@naver.com>
2025-05-29 13:20:44 +0900

    BEST button modify

--- src/pages/Subpage.jsx
+++ src/pages/Subpage.jsx
@@ -15,7 +15,6 @@ const Subpage = () => {
   const { sideMenuOpen } = useSideMenuStore();
   const isMobile = useIsMobile();
 
-  // ✅ 여기에 className 조건 추가
   const className = `Subpage ${sideMenuOpen ? 'width' : ''} ${
     isMobile && [4, 5, 6, 7].includes(pageId) ? 'mobile_common' : `mobile_${pageId}`
   }`;
@@ -26,20 +25,28 @@ const Subpage = () => {
       <div className="inner">
         <div className="subpage_img">
           {currentData && currentData.img.length > 0 ? (
-            currentData.img.map((imgSrc, index) => (
-              <div className="img_wrap" key={index}>
-                <div className="img_box">
-                  <img src={imgSrc} alt={currentData.title?.[index] || `이미지${index + 1}`} />
-                </div>
-                <div className="txt_box">
-                  <div className="top_row">
-                    <p className="img_name">{currentData.title?.[index] || ''}</p>
-                    <p className="img_price">{currentData.price?.[index] || ''}</p>
+            currentData.img.map((imgSrc, index) => {
+              const isBest = pageId === 3 && [2, 10, 13, 16, 21].includes(index);
+              const isBest2 = pageId === 4 && [0, 5, 14].includes(index);
+              const isBest3 = pageId === 6 && [5].includes(index);
+              const isBest4 = pageId === 7 && [7, 9].includes(index);
+
+              return (
+                <div className="img_wrap" key={index}>
+                  <div className="img_box">
+                    <img src={imgSrc} alt={currentData.title?.[index] || `이미지${index + 1}`} />
+                    {(isBest || isBest2 || isBest3 || isBest4) && <span className="best_badge">BEST</span>}
                   </div>
-                  <p className="img_des">{currentData.des?.[index] || ''}</p>
+                  <div className="txt_box">
+                    <div className="top_row">
+                      <p className="img_name">{currentData.title?.[index] || ''}</p>
+                      <p className="img_price">{currentData.price?.[index] || ''}</p>
+                    </div>
+                    <p className="img_des">{currentData.des?.[index] || ''}</p>
+                  </div>
                 </div>
-              </div>
-            ))
+              );
+            })
           ) : (
             <p>해당 데이터가 없습니다.</p>
           )}
